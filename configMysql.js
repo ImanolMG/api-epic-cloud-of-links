197c1c87b70b2c80ad8762626d7898627b50f291
201269ImanolMG <201269@ids.upchiapas.edu.mx>
2022-09-27 17:36:56 -0500

    cambios

--- configMysql.js
+++ configMysql.js
@@ -2,10 +2,10 @@ const mysql = require('mysql');
 
 
 const config = {
-    host : 'localhost',
-    user : 'root',
-    database: 'epic-cloud-of-links-bd-pruebas',
-    password: 'Hacker2020',
+    host : '54.90.160.31',
+    user : 'imanol',
+    database: 'dbecol',
+    password: 'password',
 };
 
 const conn = mysql.createConnection(config);
