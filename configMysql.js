af62f2ec4d4beffe5a8fe1fd5662e96397b0746d
201269ImanolMG <201269@ids.upchiapas.edu.mx>
2022-09-27 18:02:58 -0500

    cambios

--- configMysql.js
+++ configMysql.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 
 
 const config = {
-    host : '54.90.160.31',
+    host : '54.226.53.92',
     user : 'imanol',
     database: 'dbecol',
     password: 'password',
